03d1c7508a7c89e29f1e4944b8628b089d8de541
Thanh Nguyen <forenamethanh@gmai.com>
2026-02-26 00:07:35 +0700

    update s3

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket       = "dev-aman-tf-bucket"
+    bucket       = "amzn-s3-jenkins-server"
     region       = "us-east-1"
-    key          = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/EKS-TF/terraform.tfstate"
+    key          = "amzn-s3-jenkins-server/terraform.tfstate"
     use_lockfile = true
     encrypt      = true
   }
